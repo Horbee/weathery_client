1390e429b46795139fc968d86932cbc1d0ec666c
Norbert Horgas <norbert.horgas@externals.adidas.com>
2020-06-06 11:54:16 +0200

    fix backendURL bug

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -2,4 +2,4 @@ export const openWeatherMapIconURL = (icon: string) =>
   `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
 export const backendURL = (url: string) =>
-  process.env.REACT_APP_API_URL || "http://localhost:5000" + url;
+  (process.env.REACT_APP_API_URL || "http://localhost:5000") + url;
