a5d654b31876a910277a81e691fc063bdc791a9e
Horbee <n.horox@gmail.com>
2020-06-24 11:26:01 +0200

    fix authService loading property

--- src/service/auth/useAuthService.ts
+++ src/service/auth/useAuthService.ts
@@ -45,40 +45,47 @@ export const useAuthService = () => {
   const login = async (email: string, password: string) => {
     setLoading(true);
 
-    const response = await axiosInstance.post<AuthResponse>("/api/auth/login", {
-      email,
-      password
-    });
-    const token = response.data.data;
-    const decoded = jwt.decode(token) as AccessTokenProps;
-    TypedStorage.username = email;
-    TypedStorage.accessToken = token;
-    TypedStorage.tokenExpirationDate = moment(decoded.exp * 1000);
-    setAuth({
-      isLoggedIn: true,
-      accessToken: token,
-      city: decoded.user.city ?? null
-    });
-
-    setLoading(false);
+    try {
+      const response = await axiosInstance.post<AuthResponse>(
+        "/api/auth/login",
+        {
+          email,
+          password
+        }
+      );
+      const token = response.data.data;
+      const decoded = jwt.decode(token) as AccessTokenProps;
+      TypedStorage.username = email;
+      TypedStorage.accessToken = token;
+      TypedStorage.tokenExpirationDate = moment(decoded.exp * 1000);
+      setAuth({
+        isLoggedIn: true,
+        accessToken: token,
+        city: decoded.user.city ?? null
+      });
+    } finally {
+      setLoading(false);
+    }
   };
 
   const signup = async (name: string, email: string, password: string) => {
     setLoading(true);
 
-    const response = await axiosInstance.post<AuthResponse>("/api/auth", {
-      email,
-      password,
-      name
-    });
-    const token = response.data.data;
-    const decoded = jwt.decode(token) as AccessTokenProps;
-    TypedStorage.username = email;
-    TypedStorage.accessToken = token;
-    TypedStorage.tokenExpirationDate = moment(decoded.exp * 1000);
-    setAuth({ isLoggedIn: true, accessToken: token, city: null });
-
-    setLoading(false);
+    try {
+      const response = await axiosInstance.post<AuthResponse>("/api/auth", {
+        email,
+        password,
+        name
+      });
+      const token = response.data.data;
+      const decoded = jwt.decode(token) as AccessTokenProps;
+      TypedStorage.username = email;
+      TypedStorage.accessToken = token;
+      TypedStorage.tokenExpirationDate = moment(decoded.exp * 1000);
+      setAuth({ isLoggedIn: true, accessToken: token, city: null });
+    } finally {
+      setLoading(false);
+    }
   };
 
   const checkLocalStorage = () => {
@@ -116,12 +123,14 @@ export const useAuthService = () => {
   const forgotPassword = async (email: string) => {
     setLoading(true);
 
-    const response = await axiosInstance.post("/api/auth/forgotpassword", {
-      email
-    });
-    createSuccessToast(response.data.data);
-
-    setLoading(false);
+    try {
+      const response = await axiosInstance.post("/api/auth/forgotpassword", {
+        email
+      });
+      createSuccessToast(response.data.data);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const resetPassword = async (
@@ -150,64 +159,57 @@ export const useAuthService = () => {
     googleResponse: GoogleLoginResponse | GoogleLoginResponseOffline
   ) => {
     const idToken = (googleResponse as GoogleLoginResponse).tokenObj.id_token;
-    //const accessToken = (response as GoogleLoginResponse).tokenObj.access_token;
-    // const expiresAt = (response as GoogleLoginResponse).tokenObj.expires_at;
     const email = (googleResponse as GoogleLoginResponse).profileObj.email;
     const name = (googleResponse as GoogleLoginResponse).profileObj.name;
-    //const googleId = (response as GoogleLoginResponse).profileObj.googleId;
 
-    const response = await axiosInstance.post<AuthResponse>(
-      "/api/oauth/google",
-      {
-        email,
-        name,
-        idToken
-      }
-    );
-    const token = response.data.data;
-    const decoded = jwt.decode(token) as AccessTokenProps;
-    setAuth({
-      isLoggedIn: true,
-      accessToken: token,
-      city: decoded.user.city ?? null
-    });
-    // TypedStorage.username = email;
-    TypedStorage.accessToken = token;
-    TypedStorage.tokenExpirationDate = moment(decoded.exp * 1000);
-    setLoading(false);
+    try {
+      const response = await axiosInstance.post<AuthResponse>(
+        "/api/oauth/google",
+        {
+          email,
+          name,
+          idToken
+        }
+      );
+      const token = response.data.data;
+      const decoded = jwt.decode(token) as AccessTokenProps;
+      setAuth({
+        isLoggedIn: true,
+        accessToken: token,
+        city: decoded.user.city ?? null
+      });
+      TypedStorage.accessToken = token;
+      TypedStorage.tokenExpirationDate = moment(decoded.exp * 1000);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const facebookLogin = async (facebookResponse: FacebookLoginResponse) => {
     setLoading(true);
+    const { accessToken, email, name } = facebookResponse;
 
-    // console.log(facebookResponse);
-    const {
-      accessToken,
-      /*id: facebookId,*/ email,
-      name
-      // expiresIn
-    } = facebookResponse;
-
-    const response = await axiosInstance.post<AuthResponse>(
-      "/api/oauth/facebook",
-      {
-        email,
-        name,
-        accessToken
-      }
-    );
-    const token = response.data.data;
-    const decoded = jwt.decode(token) as AccessTokenProps;
-    setAuth({
-      isLoggedIn: true,
-      accessToken: token,
-      city: decoded.user.city ?? null
-    });
-    // TypedStorage.username = email!;
-    TypedStorage.accessToken = token;
-    TypedStorage.tokenExpirationDate = moment(decoded.exp * 1000);
-
-    setLoading(false);
+    try {
+      const response = await axiosInstance.post<AuthResponse>(
+        "/api/oauth/facebook",
+        {
+          email,
+          name,
+          accessToken
+        }
+      );
+      const token = response.data.data;
+      const decoded = jwt.decode(token) as AccessTokenProps;
+      setAuth({
+        isLoggedIn: true,
+        accessToken: token,
+        city: decoded.user.city ?? null
+      });
+      TypedStorage.accessToken = token;
+      TypedStorage.tokenExpirationDate = moment(decoded.exp * 1000);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return {
