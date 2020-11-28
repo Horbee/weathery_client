74c2dd8b0636f0e5828c9628b330541b4ba45eed
Norbert Horgas <n.horox@gmail.com>
2020-11-28 19:51:03 +0100

    feat: add cordova specific settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { App } from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const renderReactDom = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+if ((window as any).cordova) {
+  document.addEventListener(
+    "deviceready",
+    () => {
+      renderReactDom();
+    },
+    false
+  );
+} else {
+  renderReactDom();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
